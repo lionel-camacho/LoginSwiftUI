ad59d26a5d498ac5d3b0e84c44a2d33152e7bb63
Lionel Camacho <lionelcamacho07@gmail.com>
2021-01-19 10:12:24 +0200

    Implement error handling on users list screen

--- LoginSwiftUI/Users/UsersListView.swift
+++ LoginSwiftUI/Users/UsersListView.swift
@@ -6,8 +6,29 @@ struct UsersListView<ViewModel: UsersListViewModelProtocol>: View {
     
     var body: some View {
         NavigationView {
-            Text("Placeholder")
+            failedToLoadUsersView
+                .isHidden(!viewModel.errorOccured)
+            ProgressView(viewModel.fetchingUsersTitle)
+                .isHidden(!viewModel.isLoadingUsers)
         }
+        .navigationBarBackButtonHidden(true)
+    }
+    
+    var failedToLoadUsersView: some View {
+        VStack(alignment: .center, spacing: 15) {
+            Text(viewModel.failedToLoadUsersTitle)
+                .font(.headline)
+            Text(viewModel.failedToLoadUsersMessage)
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+            Button(action: {
+                viewModel.getUsers { (error) in
+                }
+            }) {
+                Text(viewModel.retryButtonTitle)
+                    .font(.callout)
+            }
+        } .padding()
     }
 }
 
--- LoginSwiftUI/Users/UsersListViewModel.swift
+++ LoginSwiftUI/Users/UsersListViewModel.swift
@@ -3,8 +3,14 @@ import SwiftUI
 
 protocol UsersListViewModelProtocol: ViewModelProtocol {
     var networkManager: NetworkManager { get }
+    var failedToLoadUsersTitle: String { get }
+    var failedToLoadUsersMessage: String { get }
+    var retryButtonTitle: String { get }
+    var fetchingUsersTitle: String { get }
     
     var usersResponse: [User]? { get set }
+    var isLoadingUsers: Bool { get set }
+    var errorOccured: Bool { get set }
     
     func getUsers(completion: @escaping (_ error: String?)->())
 }
@@ -12,25 +18,44 @@ protocol UsersListViewModelProtocol: ViewModelProtocol {
 class UsersListViewModel: UsersListViewModelProtocol {
     
     let networkManager = NetworkManager()
+    let failedToLoadUsersTitle = "Failed to load users"
+    let failedToLoadUsersMessage = "An error occured while attempting to load users, please ensure you are connected to wifi or a mobile connection and try again."
+    let retryButtonTitle = "Retry"
+    let fetchingUsersTitle = "Fetching users…"
     
     var usersResponse: [User]? = nil
     
+    @Published var isLoadingUsers = false
+    @Published var errorOccured = false
+    
     required init() {
-        getUsers { (error) in
-            //TODO: Handle error
-        }
+        getUsers { (error) in }
     }
     
     func getUsers(completion: @escaping (String?) -> ()) {
+        isLoadingUsers = true
+        errorOccured = false
         networkManager.getUsers { [weak self] (response, error) in
+            DispatchQueue.main.async { [weak self] in
+                self?.isLoadingUsers = false
+            }
             if error != nil {
+                DispatchQueue.main.async { [weak self] in
+                    self?.errorOccured = true
+                }
                 completion(error)
                 return
             }
             if let users = response {
                 self?.usersResponse = users
+                DispatchQueue.main.async { [weak self] in
+                    self?.errorOccured = false
+                }
                 completion(nil)
                 return
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.errorOccured = true
             }
             completion(NetworkManager.NetworkResponse.noData.rawValue)
         }
